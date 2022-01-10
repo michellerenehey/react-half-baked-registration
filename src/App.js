6f4cbec3f720d1a420d8bf90068ca536274fee33
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-01-10 12:25:29 -0800

    set up skeleton file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Auth from '../src/views/Auth';
 
 function App() {
+  // add current user state
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>i am the app component</p>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/auth">
+            <Auth />
+          </Route>
+          <Route exact path="/" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
